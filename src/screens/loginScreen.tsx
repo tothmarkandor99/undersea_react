48e34e559a7cf4f9a8a378d110b0ff7350f18f02
Tóth Márk <toth.mark@ENCOSOFT.INTERNAL>
2020-07-07 14:33:01 +0200

    Much prettier

--- src/screens/loginScreen.tsx
+++ src/screens/loginScreen.tsx
@@ -1,23 +1,37 @@
-import { View, Button, Text, TouchableWithoutFeedback, TextInput } from "react-native";
-import React from 'react';
+import {
+  View,
+  Button,
+  Text,
+  TouchableWithoutFeedback,
+  TextInput,
+} from 'react-native'
+import React from 'react'
+import {StackNavigationProp} from '@react-navigation/stack'
+
+interface LoginScreenProps {
+  navigation: StackNavigationProp<any>
+}
 
 export default LoginScreen
-function LoginScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Undersea</Text>
-        <View>
-            <Text>Belépés</Text>
-            <TextInput placeholder="Felhasználónév"/>
-            <TextInput placeholder="Jelszó"/>
-            <Button title="Belépés" onPress={()=>{}}/>
-            <Text>
-                Nincs még fiókod?
-                <TouchableWithoutFeedback onPress={() => {navigation.navigate("Register")}}>
-                    <Text>Regisztrálj</Text>
-                </TouchableWithoutFeedback>
-            </Text>
-        </View>
+function LoginScreen({navigation}: LoginScreenProps) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Undersea</Text>
+      <View>
+        <Text>Belépés</Text>
+        <TextInput placeholder="Felhasználónév" />
+        <TextInput placeholder="Jelszó" />
+        <Button title="Belépés" onPress={() => {}} />
+        <Text>
+          Nincs még fiókod?
+          <TouchableWithoutFeedback
+            onPress={() => {
+              navigation.navigate('Register')
+            }}>
+            <Text>Regisztrálj</Text>
+          </TouchableWithoutFeedback>
+        </Text>
       </View>
-    );
-  }
+    </View>
+  )
+}
--- src/screens/registerScreen.tsx
+++ src/screens/registerScreen.tsx
@@ -1,25 +1,39 @@
-import { View, Button, Text, TouchableWithoutFeedback, TextInput } from "react-native";
-import React from 'react';
+import {
+  View,
+  Button,
+  Text,
+  TouchableWithoutFeedback,
+  TextInput,
+} from 'react-native'
+import React from 'react'
+import {StackNavigationProp} from '@react-navigation/stack'
+
+interface LoginScreenProps {
+  navigation: StackNavigationProp<any>
+}
 
 export default LoginScreen
-function LoginScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Undersea</Text>
-        <View>
-            <Text>Regisztráció</Text>
-            <TextInput placeholder="Felhasználónév"/>
-            <TextInput placeholder="Jelszó"/>
-            <TextInput placeholder="Jelszó megerősítése"/>
-            <TextInput placeholder="A városod neve, amit építesz"/>
-            <Button title="Regisztráció" onPress={()=>{}}/>
-            <Text>
-                Már van fiókod?
-                <TouchableWithoutFeedback onPress={() => {navigation.navigate("Register")}}>
-                    <Text>Lépj be</Text>
-                </TouchableWithoutFeedback>
-            </Text>
-        </View>
+function LoginScreen({navigation}: LoginScreenProps) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Undersea</Text>
+      <View>
+        <Text>Regisztráció</Text>
+        <TextInput placeholder="Felhasználónév" />
+        <TextInput placeholder="Jelszó" />
+        <TextInput placeholder="Jelszó megerősítése" />
+        <TextInput placeholder="A városod neve, amit építesz" />
+        <Button title="Regisztráció" onPress={() => {}} />
+        <Text>
+          Már van fiókod?
+          <TouchableWithoutFeedback
+            onPress={() => {
+              navigation.navigate('Register')
+            }}>
+            <Text>Lépj be</Text>
+          </TouchableWithoutFeedback>
+        </Text>
       </View>
-    );
-  }
+    </View>
+  )
+}

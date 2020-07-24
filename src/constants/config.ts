3fa6816e2f2753ce7afa8c89a5adb17de39f191a
tothmarkandor99 <tothmarkandor99@freemail.hu>
2020-07-24 12:53:54 +0200

    Logging turned on

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -14,10 +14,10 @@ interface ConfigProps {
 export const Config: ConfigProps = {
   baseUrl: 'http://underseat2lasttry.webtest.encosoft.internal/api',
   resourceUrl: 'http://underseat2lasttry.webtest.encosoft.internal',
-  loggingRedux: 'none',
-  loggingRequests: false,
-  loggingResponses: false,
-  loggingSignalR: false,
+  loggingRedux: 'names',
+  loggingRequests: true,
+  loggingResponses: true,
+  loggingSignalR: true,
   defaultSearchItemsPerPage: 10,
   signalRLogLevel: signalR.LogLevel.None,
 }
